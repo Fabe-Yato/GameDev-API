922de03a0a3d1a32a2344f9b1be5285fefdbf92d
Fabiano <Fabibinho2@gmail.com>
2022-12-01 00:38:45 -0300

    arrumando o problema das capturas

--- src/capturas_tela/entities/capturas.entity.ts
+++ src/capturas_tela/entities/capturas.entity.ts
@@ -1,35 +1,39 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { MaxLength, IsNotEmpty } from 'class-validator'
-import { Jogos } from 'src/jogos/entities/jogos.entity'
-import {Entity, PrimaryGeneratedColumn, Column, ManyToMany, JoinTable} from 'typeorm'
+import { ApiProperty } from '@nestjs/swagger';
+import { MaxLength, IsNotEmpty } from 'class-validator';
+import { Jogos } from 'src/jogos/entities/jogos.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm';
 
-@Entity({name: "tb_capturas_tela"})
-export class Capturas{
+@Entity({ name: 'tb_capturas_tela' })
+export class Capturas {
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  id_imagens: number;
 
-    @ApiProperty()
-    @PrimaryGeneratedColumn()
-    id_imagens: number
+  @ApiProperty()
+  @MaxLength(300)
+  @Column()
+  nome_jogo: string;
 
-    @ApiProperty()
-    @MaxLength(300)
-    @Column()
-    nome_jogo: string
+  @ApiProperty()
+  @IsNotEmpty()
+  @MaxLength(2000)
+  @Column()
+  imagem1: string;
 
-    @ApiProperty()
-    @IsNotEmpty()
-    @MaxLength(2000)
-    @Column()
-    imagem1: string
+  @ApiProperty()
+  @IsNotEmpty()
+  @MaxLength(2000)
+  @Column()
+  imagem2: string;
 
-    @ApiProperty()
-    @IsNotEmpty()
-    @MaxLength(2000)
-    @Column()
-    imagem2: string
-
-    @ManyToMany(() => Jogos, (jogos) => jogos.capturas)
-    @JoinTable()
-    @ApiProperty({type: ()=> Jogos})
-    jogos: Jogos
-
+  @ManyToMany(() => Jogos, (jogos) => jogos.capturas)
+  @JoinTable()
+  @ApiProperty({ type: () => Jogos })
+  jogos: Jogos;
 }
